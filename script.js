08ad9ba5214a33d8a9f86082f665d08b778171d3
Naveenkarthik03 <naveenkarthik77@gmail.com>
2026-06-17 18:21:26 +0530

    added typing

--- script.js
+++ script.js
@@ -41,3 +41,61 @@ hiddenElements.forEach((element) => {
     observer.observe(element);
 
 });
+
+const typingText = document.getElementById("typing-text");
+
+const words = [
+
+    "Software Engineer",
+
+    "Full Stack Developer",
+
+    "Python Developer"
+
+];
+
+let wordIndex = 0;
+
+let charIndex = 0;
+
+let isDeleting = false;
+
+function typeEffect(){
+
+    const currentWord = words[wordIndex];
+
+    if(isDeleting){
+
+        typingText.textContent = currentWord.substring(0,charIndex--);
+
+    }
+
+    else{
+
+        typingText.textContent = currentWord.substring(0,charIndex++);
+
+    }
+
+    let speed = isDeleting ? 60 : 120;
+
+    if(!isDeleting && charIndex === currentWord.length + 1){
+
+        speed = 1500;
+
+        isDeleting = true;
+
+    }
+
+    if(isDeleting && charIndex === 0){
+
+        isDeleting = false;
+
+        wordIndex = (wordIndex + 1) % words.length;
+
+    }
+
+    setTimeout(typeEffect,speed);
+
+}
+
+typeEffect();
